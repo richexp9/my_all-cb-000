ab5e2f238869951ad56402815533c2cc41a8f713
Richard Loera <richexp9@gmx.com>
2018-01-14 23:56:20 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
-def my_all?(collection)
+def my_all?(array)
+  i = 0
+  collection =[]
+  while i < collection.size
 
+    collection<<yield(array[i])
+    i += 1
+  end
+  collection 
 end
